b2902a924f4816d2b93d5510191bef50f32e521c
DHARAMPAL SINGH <ravisingh02082001@gmail.com>
2023-04-01 16:24:24 +0530

    Update useFetch.js

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -10,8 +10,7 @@ const useFetch = (endpoint, query) => {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      // "X-RapidAPI-Key": 'de39803e94msh234c92ea368a8d2p1a57d9jsn548078485e22',
-      "X-RapidAPI-Key": 'KJWZZIJSFimshuivMSVGaiYzkRomp15f2vKjsnK4bKzuUZVLZA',
+      "X-RapidAPI-Key": 'YOUR_API_KEY',
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
     params: { ...query },
